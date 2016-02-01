af56672927b77ccc51c9b51753225b85e362fb81
joelcw <joel.wallenberg@gmail.com>
2016-02-01 18:37:29 +0000

    fixed script

--- scripts/extractum.py
+++ scripts/extractum.py
@@ -10,6 +10,14 @@ digit = float(sys.argv[2])
 
 gender = float(sys.argv[4])
 
+#Assumed to be Kinsey number:
+
+sexuality = sys.argv[5]
+
+#Assumed to be binary:
+
+tomboy = sys.argv[6]
+
 interviewer = sys.argv[3]
 
 #logdigit = math.log(digit,2)
@@ -18,7 +26,9 @@ lines = sys.stdin.readlines()
 
 #This assumes that the interviewee name appears in the second field of the first line; ELAN output needs to be set up to output the interviewee lines first, or else this is not helpful.
 
-name = lines[0].split()[1]
+#Assumes the name of the interviewee can be found in the first field of the first line. Make sure ELAN output is configured accordingly.
+
+name = lines[0].split()[0]
 #name = "CT"
 
 interviewee = []
@@ -49,17 +59,19 @@ def comma(matchobj):
 
 #Makes a list of all lines spoken by the interviewee, ignoring interviewer speech
 
+#Assumes the name of the interviewee can be found in the first field of the first line. Make sure ELAN output is configured accordingly.
+
 for li in lines:
-    if li.split()[1] == name:
+    if li.split()[0] == name:
         interviewee.append(li)
 
-#Get a wordcount, before splitting off commas/periods. The -4 is to remove the metadata fields; this assumes that ELAN is outputting 4 metadata fields at the beginning of each line.
+#Get a wordcount, before splitting off commas/periods. The -3 is to remove the metadata fields; this assumes that ELAN is outputting 3 metadata fields at the beginning of each line.
 
 wordcount = 0
 
 for line in interviewee:
     words = line.split()
-    wordcount = wordcount+len(words)-4
+    wordcount = wordcount+len(words)-3
 
 
 
@@ -72,8 +84,8 @@ for line in interviewee:
         matches = filledpause.search(w)
         if matches != None:
             sys.stdout.write("%s,%s," % (name,matches.group(0).lower()))
-#Again, the ii==4 is assuming ELAN has output 4 columns of metadata; if this changes you, you have to change this. It's there so that um/uh at the beginning of an utterance is coded as having a preceding pause.
-            if (ii == 4) or (precpause.search(words[ii-1]) != None):
+#Again, the ii==3 is assuming ELAN has output 3 columns of metadata; if this changes you, you have to change this. It's there so that um/uh at the beginning of an utterance is coded as having a preceding pause.
+            if (ii == 3) or (precpause.search(words[ii-1]) != None):
                 sys.stdout.write("P,")
             else:
                 sys.stdout.write("S,")
@@ -81,5 +93,5 @@ for line in interviewee:
                 sys.stdout.write("P,")
             else:
                 sys.stdout.write("S,")
-            sys.stdout.write("%s,%s,%s,%s,%s\n" % (age, gender, digit, interviewer, wordcount))
+            sys.stdout.write("%s,%s,%s,%s,%s,%s,%s\n" % (age, digit, interviewer, wordcount, gender, sexuality, tomboy))
         ii = ii+1
