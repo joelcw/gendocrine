64afce14733fd62eed753ea9f8ae59f05b202417
joelcw <joel.wallenberg@gmail.com>
2016-11-03 16:16:59 +0000

    added figures

--- R/tomboyAnalysis.R
+++ R/tomboyAnalysis.R
@@ -2,7 +2,8 @@ library(ggplot2)
 library(plyr)
 library(dplyr)
 library(lme4)
-
+library(RColorBrewer)
+library(gam)
 
 tomboys <- read.csv("gendocrine/datasets/BethTomboyDataset.csv")
 
@@ -44,3 +45,19 @@ anova(tomboys.fit2, test = "Chisq")
 tomboys.fit3 <- glm(yesOrNo~lnLeft, family = binomial, data=tomboys)
 summary(tomboys.fit3)
 anova(tomboys.fit3, test = "Chisq")
+
+
+#plot, unbinned data, with logistic and gam
+
+tomboys$test <- tomboys$yesOrNo
+
+p <- ggplot(tomboys, aes(lnRight, yesOrNo)) + scale_y_continuous(name = "Probability: Tomboy Or Not?", breaks=seq(0,1,by=0.1), labels=c("Not Tomboy",seq(0.1,0.9,by = 0.1),"Tomboy") ) + scale_x_continuous(name = "\nln(right hand 2D:4D ratio)")  + geom_point() + scale_alpha_continuous(guide="none", limits = c(0,.9))  + theme_bw() + theme(panel.border = element_blank()) + scale_color_brewer(palette = "Dark2") + geom_jitter(height = 0.1) + geom_smooth(alpha = 0.2, method="glm",method.args = list(family = "binomial"),fullrange=T) 
+
+ggsave(p, file = "~/gendocrine/tomboysUnbinnedLogistic.pdf", width = 8, height = 5)
+
+#fuckin around with splines
+
+library(splines)
+library(MASS)
+
+ggplot(tomboys, aes(lnRight, test)) + scale_y_continuous(name = "Probability: Tomboy Or Not?", breaks=seq(0,1,by=0.1), labels=c("Not Tomboy",seq(0.1,0.9,by = 0.1),"Tomboy") ) + scale_x_continuous(name = "\nln(right hand 2D:4D ratio)")  + geom_smooth(alpha = 0.2, method="gam",formula = y ~ ns(x,3),fullrange=T) + scale_alpha_continuous(guide="none", limits = c(0,.9)) + scale_color_brewer(palette = "Dark2") + theme_bw() + theme(panel.border = element_blank()) + geom_point() 
